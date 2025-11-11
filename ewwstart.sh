6600378018820e3745678529281696f5da375c85
Julia-Sidequest <Malmberg403@proton.me>
2025-11-11 22:48:52 +0100

    Add files via upload
    should start all the instances off eww automatically

---
+++ ewwstart.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+~/eww/target/release/eww daemon
+~/eww/target/release/eww open clock
+~/eww/target/release/eww open playpause
+~/eww/target/release/eww open text
+~/eww/target/release/eww open music
+~/eww/target/release/eww open placeholder 
+~/eww/target/release/eww open hii
+~/eww/target/release/eww open weather
+~/eww/target/release/eww open pink
+~/eww/target/release/eww open volume
+
